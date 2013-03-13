4e901b82c25ec6bf16fca205cca63d769e4b9b2b
glaslos <glaslos@gmail.com>
2013-03-13 17:54:18 +0100

    gevent UDP server testing

--- run.py
+++ run.py
@@ -1,37 +1,24 @@
-################################################################################
-#
-# Stand-alone VoIP honeypot client (preparation for Dionaea integration)
-# Copyright (c) 2010 Tobias Wulff (twu200 at gmail)
-#
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
-# Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
-################################################################################
-
-import asyncore
 import logging
+
+import gevent
 
 import sip
 
+
 # Setup logging mechanism
 logger = logging.getLogger('sip')
-logger.setLevel(logging.DEBUG)
 logConsole = logging.StreamHandler()
 logConsole.setLevel(logging.DEBUG)
 logConsole.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
 logger.addHandler(logConsole)
+
+
+class SIPServer(DatagramServer):
+
+    def handle(self, data, address):
+        print '%s: got %r' % (address[0], data)
+        self.socket.sendto('Received %s bytes' % len(data), address)
+
 
 if __name__ == '__main__':
     s = sip.SipSession(proto="udp")
@@ -39,8 +26,6 @@ if __name__ == '__main__':
 
     try:
         print "Looping now..."
-        asyncore.loop()
+        SIPServer('localhost:5060').serve_forever()
     except KeyboardInterrupt:
         print("Closing socket...")
-        s.close()
-        asyncore.loop()
